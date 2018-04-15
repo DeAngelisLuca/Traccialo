d7b5be46d480182f876bc7e730597538c4771cde
Luca <Luca@PC-Luca.homenet.telecomitalia.it>
2018-04-15 19:34:30 +0200

    test db peristence

--- Traccialo/src/main/java/it/deangelis/Traccialo/TraccialoApplication.java
+++ Traccialo/src/main/java/it/deangelis/Traccialo/TraccialoApplication.java
@@ -1,8 +1,15 @@
 package it.deangelis.Traccialo;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import it.deangelis.model.Prodotto;
+
+
 
 
 
@@ -11,6 +18,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TraccialoApplication {
 
 	public static void main(String[] args) {
+		SessionFactory sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		
+		Prodotto p1 = new Prodotto("aa", "aaaa", "cat", 2.0);
+		session.save(p1);
+	
+		
+ 
+		session.getTransaction().commit();
+		session.close();
+		
 		SpringApplication.run(TraccialoApplication.class, args);
 		
 
